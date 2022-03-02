24ba85327045b94dfae49efb67ee213942524d43
Sagar Saxena <ssaxena1@umd.edu>
2022-03-02 10:39:15 -0500

    parent repo compatibility

--- onpolicy/__init__.py
+++ onpolicy/__init__.py
@@ -1,4 +1,4 @@
-from onpolicy import algorithms, envs, runner, scripts, utils, config
+from src.models.onpolicy.onpolicy import algorithms, envs, runner, scripts, utils, config
 
 
 __version__ = "0.1.0"
